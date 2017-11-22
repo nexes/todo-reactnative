9ebf73b5c04e2ddf60822ed8d0620725938bb6ad
Joe Berria <joeberria@gmail.com>
2017-11-22 09:12:10 -0800

    update todo list item component, allow changing todo item text

--- app/component/todoitem/listitem.js
+++ app/component/todoitem/listitem.js
@@ -1,21 +1,44 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Style } from './style';
+import { Checkbox } from '../checkbox';
 import {
   View,
-  Text
+  TextInput
 } from 'react-native';
 
 
-export default class TodoItem extends React.Component {
+export default class TodoItem extends React.PureComponent {
   constructor(props) {
     super(props);
+    this.itemEdit = this.itemEdit.bind(this);
+  }
+
+  itemEdit(text) {
+    this.props.valueChange(text, this.props.index);
   }
 
   render() {
     return (
-      <View style={[ this.props.style ]}>
-        <Text>hello</Text>
+      <View style={[ Style.container, this.props.style ]}>
+        <Checkbox />
+        <TextInput
+          style={Style.todoText}
+          value={this.props.value}
+          multiline={false}
+          editable={true}
+          autoCapitalize='none'
+          autoCorrect={false}
+          onChangeText={this.itemEdit}
+        />
       </View>
     );
   }
 }
+
+
+TodoItem.propTypes = {
+  index: PropTypes.number.isRequired,
+  value: PropTypes.string.isRequired,
+  valueChange: PropTypes.func.isRequired
+};
--- app/component/todoitem/style.js
+++ app/component/todoitem/style.js
@@ -2,5 +2,16 @@ import { StyleSheet } from 'react-native';
 
 
 export const Style = StyleSheet.create({
-
+  container: {
+    marginLeft: 10,
+    padding: 5,
+    flexDirection: 'row',
+  },
+  todoText: {
+    width: '100%',
+    height: 30,
+    fontSize: 16,
+    paddingLeft: 10,
+    paddingTop: 3
+  }
 });
