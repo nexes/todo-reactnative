f774643c5f9220880b277eab2c1dc43e3e9c7e72
Joe Berria <joeberria@gmail.com>
2017-11-22 13:56:12 -0800

    update to navigation

--- app/routes.js
+++ app/routes.js
@@ -1,10 +1,30 @@
 import { TabNavigator, StackNavigator } from 'react-navigation';
 import { Today } from './screens/today';
+import { NewTodoScreen } from './screens/newitem';
 
+
+const myDayNavigator = StackNavigator({
+  Home: {
+    screen: Today,
+    navigationOptions: {
+      headerStyle: {display: 'none'},
+    }
+  },
+  AddItem: {
+    screen: NewTodoScreen,
+    navigationOptions: {
+      headerLeft: null,
+      title: 'New todo',
+    }
+  }
+},{
+  initialRouteName: 'Home',
+  mode: 'modal',
+});
 
 const tabNavigator = TabNavigator({
   Today: {
-    screen: Today
+    screen: myDayNavigator
   }
 });
 
