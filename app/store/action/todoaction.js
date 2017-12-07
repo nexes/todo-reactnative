cd486586ab869f965a027297bf298fb86e944782
Joe Berria <joeberria@gmail.com>
2017-12-07 11:45:40 -0800

    update to store action, including initial todo item complete action

--- app/store/action/todoaction.js
+++ app/store/action/todoaction.js
@@ -1,22 +1,45 @@
 export const ActionType = {
   ADD_TODO: 'add_todo',
-  REMOVE_TODO: 'remove_todo'
+  REMOVE_TODO: 'remove_todo',
+  COMPLETE_TODO: 'complete_todo',
 };
 
 
+/**
+ * add a new todo item to the store
+ * @param {object} todoObj  the new todo object, keys - text: string, date: date, category: string, note: string
+ * @return {action}      the action object used by the reducers
+ */
 export function addTodo(todoObj) {
   return {
     type: ActionType.ADD_TODO,
     text: todoObj.text,
     date: todoObj.due,
     category: todoObj.category,
-    note: todoObj.note
+    note: todoObj.note,
   };
 }
 
+/**
+ * remove a todo item by the the todo text
+ * @param  {string} text the todo text that will be removed from the list of todos
+ * @return {action}      the action object used by the reducers
+ */
 export function removeTodo(text) {
   return {
     type: ActionType.REMOVE_TODO,
-    text
+    text,
+  };
+}
+
+/**
+ * set a todo item as completed
+ * @param  {string} text the todo text that will be marked as complete
+ * @return {action}      the action object used by the reducers
+ */
+export function completeTodo(text) {
+  return {
+    type: ActionType.COMPLETE_TODO,
+    text,
   };
 }
--- app/store/store.js
+++ app/store/store.js
@@ -10,14 +10,15 @@ const initialState = {
         text: the todo title,
         note: the todo detail if any,
         due: the due date if any,
-        category: the category of the todo item if any
+        category: the category of the todo item if any,
+        completed: if the todo is finished
       }
     */
-  ]
+  ],
 };
 
 export const store = createStore(combineReducers({
-  todo: todoItem
+  todo: todoItem,
 }), initialState);
 
 export { actions as Action };
