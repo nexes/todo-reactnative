aa72e36a8c697f349cfc9a4ec0764a17159f2f2e
Joe Berria <joeberria@gmail.com>
2017-12-11 13:56:32 -0800

    wrapping redux store in a class to export to the app

--- app/store/store.js
+++ app/store/store.js
@@ -17,8 +17,45 @@ const initialState = {
   ],
 };
 
-export const store = createStore(combineReducers({
-  todo: todoItem,
-}), initialState);
 
+class Store {
+  constructor() {
+    this.reduxStore = createStore(combineReducers({
+      todo: todoItem,
+    }), initialState);
+
+    this.unsubscribe = undefined;
+  }
+
+  subscribe(listener) {
+    this.unsubscribe = this.reduxStore.subscribe(listener);
+  }
+
+  unSubscribe() {
+    if (this.unsubscribe) {
+      this.unsubscribe();
+    }
+  }
+
+  currentState() {
+    return this.reduxStore.getState();
+  }
+
+  dispatchAction(action) {
+    if (action) {
+      this.reduxStore.dispatch(action);
+    }
+  }
+
+  saveStateToDevice() {
+    //  TODO save redux state to the device
+  }
+
+  retrieveStateFromDevice() {
+    //  TODO get redux state from the device
+  }
+
+}
+
+export const store = new Store();
 export { actions as Action };
