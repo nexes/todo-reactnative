7a7a42d00e76cdf1e3409b5add6aee0b2ff6bf1b
Joe Berria <joeberria@gmail.com>
2017-12-20 23:12:05 -0800

    no longer need to wrap our store, using react-redux for cleaner code

--- app/store/store.js
+++ app/store/store.js
@@ -29,55 +29,10 @@ const initialState = {
 };
 
 
-class Store {
-	constructor() {
-		this.reduxStore = createStore(combineReducers({
-			todo: todoItem,
-			category: categories,
-		}), initialState);
+export const store = createStore(combineReducers({
+	todo: todoItem,
+	category: categories,
+}), initialState);
 
-		this.unsubscribe = undefined;
-	}
-
-	subscribe(listener) {
-		this.unsubscribe = this.reduxStore.subscribe(listener);
-	}
-
-	unSubscribe() {
-		if (this.unsubscribe) {
-			this.unsubscribe();
-		}
-	}
-
-	currentState() {
-		return this.reduxStore.getState();
-	}
-
-	getTodoItems() {
-		const { todo } = this.reduxStore.getState();
-		return todo;
-	}
-
-	getTodoCategories() {
-		const { category } = this.reduxStore.getState();
-		return category;
-	}
-
-	dispatchAction(action) {
-		if (action) {
-			this.reduxStore.dispatch(action);
-		}
-	}
-
-	saveStateToDevice() {
-		//  TODO save redux state to the device
-	}
-
-	retrieveStateFromDevice() {
-		//  TODO get redux state from the device
-	}
-}
-
-export const store = new Store();
 export { todActions as TodoAction };
 export { categoryAction as CategoryAction };
